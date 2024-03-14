78e94a4ec5015d50b1a54e9ee519ed3abee253e2
Talha Bin Nasir <talhaabinnasir@gmail.com>
2024-03-14 15:00:37 +0500

    Rename app

--- client/src/modules/navbar/index.tsx
+++ client/src/modules/navbar/index.tsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          ConnectWorx
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
